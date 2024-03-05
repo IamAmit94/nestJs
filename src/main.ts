8f8d06632d7be3776e52be32e83e03d4d9e5e498
IamAmit94 <amitrvt18@hotmail.com>
2024-03-05 13:52:47 +0530

    Validation and Error handling

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe()) // In order to add the validation to your whole project
   await app.listen(4000);
 }
 bootstrap();
